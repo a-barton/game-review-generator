8fbcaa7024cb34ed870cda048d6ae0df23e010b9
Andrew Barton <72054220+a-barton@users.noreply.github.com>
2021-07-24 17:56:04 +1000

    Merging feature/simple-testing-container back into main (#4)
    * Reverted to simple container Dockerfile and changed generate_review.py Container script to simply add output test string to end of prompt, and read from S3 correctly
    
    * Updated cloudformation stack to use a1.medium instance type and ap-southeast-2b AZ
    
    * Changed batch stack instance type to m3.medium, confirmed Batch job now runs successfully, and added .gitignore to repo
    
    * Removed and re-added everything to index to apply .gitignore rules

--- src/src-container/generate_review.py
+++ src/src-container/generate_review.py
@@ -1,7 +1,6 @@
 import os
 import boto3
 import io
-from transformers import pipeline
 
 bucket = os.environ.get('S3_BUCKET', None)
 inference_input_key = 'inference_input/prompt.txt'
@@ -10,14 +9,11 @@ output_key = 'inference_output/generated.txt'
 if bucket:
     s3_client = boto3.client('s3')
     obj = s3_client.get_object(Bucket=bucket, Key=inference_input_key)
-    prompt = open(io.BytesIO(obj['Body'].read())).read()
+    prompt = obj['Body'].read().decode('utf-8')
 else:
     prompt = open('../../game-review-generator/' + inference_input_key).read()
 
-generator = pipeline('text-generation', model='EleutherAI/gpt-neo-125M', model_kwargs={'max_length':1000})
-output = generator(prompt, do_sample=True, min_length=50, max_length=500)[0]['generated_text']
-
-#output = prompt + ' + output test'
+output = prompt + ' + output test'
 
 if bucket:
     s3_client.put_object(Body=output, Bucket=bucket, Key=output_key)
