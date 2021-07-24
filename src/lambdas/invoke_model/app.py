fffbae1170b60661322f2fc0c269e2f8a37f9221
Andrew Barton <72054220+a-barton@users.noreply.github.com>
2021-07-24 22:10:54 +1000

    Implemented batch job invocation into invoke_model lambda (#5)

--- src/lambdas/invoke_model/app.py
+++ src/lambdas/invoke_model/app.py
@@ -4,9 +4,16 @@ def lambda_handler(event, context):
     prompt = event['prompt']
     bucket = event['bucket']
     inference_input_key = 'inference_input/prompt.txt'
-    output_key = 'inference_output/generated.txt'
 
     s3_client = boto3.client('s3')
     s3_client.put_object(Body=prompt, Bucket=bucket, Key=inference_input_key)
 
-    return {'prompt' : prompt, 'bucket' : bucket}
+    batch_client = boto3.client('batch')
+
+    batch_job = batch_client.submit_job(
+        jobName='BatchProcessingJobQueue', 
+        jobQueue='BatchProcessingJobQueue', 
+        jobDefinition='BatchJobDefinition'
+    )
+
+    return batch_job
