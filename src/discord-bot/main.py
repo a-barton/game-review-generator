e03145386a7b053328712e97259f8d07294f7a46
Andrew Barton <andrew.barton.usyd@gmail.com>
2021-08-01 18:50:54 +1000

    Put app details retrieval process into its own function and made it more robust

--- src/discord-bot/main.py
+++ src/discord-bot/main.py
@@ -3,12 +3,13 @@ import os
 import re
 import requests
 import json
+import time
 
 from discord.errors import ClientException
 
 client = discord.Client()
 
-steam_url_regex = re.compile(r'(?:https://store.steampowered.com/app/)([0-9]+)(?:/)')
+steam_url_regex = re.compile(r"(?:https://store.steampowered.com/app/)([0-9]+)(?:/)")
 app_details_url = "http://store.steampowered.com/api/appdetails?appids="
 
 @client.event
@@ -21,24 +22,48 @@ async def on_message(message):
         return
     
     if message.content.startswith("$hello"):
-        await message.reply(f"Hello {message.author}!")
+        await message.reply(f"Hello {message.author.display_name}!")
 
     if message.content.startswith("https://store.steampowered.com/app/"):
-        print('Detected Steam URL')
+        print("Detected Steam URL")
         app_id = re.match(steam_url_regex, message.content).group(1)
-        app_details_resp = requests.get(app_details_url + app_id).json()[app_id]["data"]
-        app_desc = app_details_resp["detailed_description"]
-        app_name = app_details_resp["name"]
-        app_desc = re.sub("<[^<]+?>", " ", app_desc)
+        print("Extracted app ID from URL")
+        app_name, app_desc = get_app_details(app_id)
+        print("Retrieved app name and description from Steam:")
+        print(f"App Name = {app_name}\nApp Description = {app_desc}")
+        print("Polling transformer API to generate review")
         review = poll_transformer_api(app_desc)
-        await message.channel.send(f"Here's my take on {app_name} \n ```" + review + "```")
+        await message.reply(f"Here's my take on {app_name} \n ```" + review + "```")
+
+def get_app_details(app_id):
+    request_successful = False
+    time_started = time.time()
+    time_taken = 0
+    while not request_successful:
+        base_resp = requests.get(app_details_url + app_id)
+        time_taken = time.time() - time_started
+        if base_resp.status_code != 200:
+            if time_taken > 10:
+                print("Steam app details request timing out")
+                return None, None
+            time.sleep(0.2)
+            continue
+        else:
+            request_successful = True
+            app_details = base_resp.json()[app_id]
+            if not app_details.get("data", None):
+                return None, None
+            app_details_data = app_details["data"]
+    app_name = app_details_data["name"]
+    app_desc = re.sub("<[^<]+?>", " ", app_details_data["detailed_description"])
+    return app_name, app_desc
 
 def poll_transformer_api(prompt):
     api_endpoint = "https://api-inference.huggingface.co/models/EleutherAI/gpt-neo-2.7B"
     headers = {"Authorization": f"Bearer {os.getenv('HUGGINGFACE_API_TOKEN')}"}
     payload = {
         "inputs" : prompt,
-        'parameters' : {
+        "parameters" : {
             "return_full_text" : False,
             "max_new_tokens" : 200
         }
