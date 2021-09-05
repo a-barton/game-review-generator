7be6f59c721bd5b1793f56390fc333d479b826ee
Andrew Barton <72054220+a-barton@users.noreply.github.com>
2021-09-05 22:53:20 +1000

    Merging fix/message-length back into main (#15)
    * Changed bot to trim review length if it would result in trying to post a Discord message beyond 2000 characters.
    
    * Pulled inference hyperparameters out into separate config file within container source code

--- src/discord-bot/utils/logging.py
+++ src/discord-bot/utils/logging.py
@@ -1,17 +1,24 @@
 import logging
 import watchtower
 import requests
+import os
+
+location = os.environ.get("LOCATION", None)
 
 logging.basicConfig(level=logging.DEBUG)
 
-instance_id = requests.get('http://169.254.169.254/latest/meta-data/instance-id').text
-logger_name = f"game-review-generator-discord-bot-ec2-id-{instance_id}"
+if location == "local":
+    LOGGER = logging.getLogger("game-review-generator-local")
+    LOGGER.setLevel(logging.DEBUG)
+else:
+    instance_id = requests.get('http://169.254.169.254/latest/meta-data/instance-id').text
+    logger_name = f"game-review-generator-discord-bot-ec2-id-{instance_id}"
 
-LOGGER = logging.getLogger(logger_name)
-LOGGER.setLevel(logging.DEBUG)
+    LOGGER = logging.getLogger(logger_name)
+    LOGGER.setLevel(logging.DEBUG)
 
-handler = watchtower.CloudWatchLogHandler(
-    log_group=logger_name
-)
+    handler = watchtower.CloudWatchLogHandler(
+        log_group=logger_name
+    )
 
-LOGGER.addHandler(handler)
+    LOGGER.addHandler(handler)
